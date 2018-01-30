43a2df1227a704d6ad23a4c31a66d021f4fa232e
Daniel e. Naranjo Sampson <danielnaranjo37@gmail.com>
2018-01-29 23:40:52 -0600

    .....

--- DataTesting/ViewController.swift
+++ DataTesting/ViewController.swift
@@ -13,20 +13,46 @@ class ThisView: UIViewController {
     @IBOutlet weak var brillo: UILabel!
     @IBOutlet weak var durabilidad: UILabel!
     
+    var manzanas: [Manzana] = []
+    var yesNo = -1
+    var arr: ManzanaArray!
+    
+    @IBAction func theButton(_ sender: Any) {
+        manzanas = arr.restoreString()
+        yesNo *= -1
+        if yesNo > 0 {
+            let manzanita = manzanas[0]
+            color.text = manzanita.color
+            brillo.text = manzanita.brillo
+            durabilidad.text = manzanita.durabilidad
+            return
+        }
+        else {
+            let manzanita = manzanas[1]
+            color.text = manzanita.color
+            brillo.text = manzanita.brillo
+            durabilidad.text = manzanita.durabilidad
+            return
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let manzana = Manzana()
+        manzana.color = "black like my soul"
+        manzana.brillo = "dark as all hell"
+        manzana.durabilidad = "like a baby butt"
+        let manzana2 = Manzana()
+        
         let coder = NSKeyedArchiver()
-        let arr = ManzanaArray(names: [manzana], fileName: "manzanita")!
+        arr = ManzanaArray(names: [manzana,manzana2], fileName: "manzanita")!
+        
         arr.encode(with: coder)
         arr.archiveString(str: [manzana])
         coder.finishEncoding()
-        let manzanas = arr.restoreString()
-        let manzanita = manzanas[0]
-        color.text = manzanita.color
-        brillo.text = manzanita.brillo
-        durabilidad.text = manzanita.durabilidad
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +60,10 @@ class ThisView: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
 
 }
 
